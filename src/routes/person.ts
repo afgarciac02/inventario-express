e867d0cba9301a4d0823f7d49d15f77c6f9dbd11
afgarciac02 <afgarciac02@libertadores.edu.co>
2022-09-26 21:34:14 -0500

    fix person multiple constults

--- src/routes/person.ts
+++ src/routes/person.ts
@@ -5,11 +5,12 @@ import { person } from "../types";
 const router = express.Router();
 
 router.get('/', async (_req, res) => {
-    let inventory = await personService.getPerson()
-    if (inventory) {
-        res.json(inventory)   
-        .setHeader('Content-Type', 'application/json')
-        .sendStatus(200);
+    let person = await personService.getPerson()
+    if (person) {
+        res.setHeader('Content-Type', 'application/json')
+        .writeHead(200)
+        .write(person)
+        res.end(person)
     }
 });
 
@@ -17,9 +18,10 @@ router.get('/id/:id', async (req, res) => {
 
     let response = await personService.getPersonById(parseInt(req.params.id));
     if (response && req.params.id) {
-        res.json(response)
-        .setHeader('Content-Type', 'application/json')
-        .sendStatus(200);
+        res.setHeader('Content-Type', 'application/json')
+        .writeHead(200)
+        .write(response)
+        res.end(response)
     }
 
 })
@@ -28,20 +30,23 @@ router.get('/name/:name', async (req, res) => {
     
         let response = await personService.getPersonByName(req.params.name);
         if(response && req.params.name){
-            res.json(response)
-            .setHeader('Content-Type', 'application/json')
-            .sendStatus(200);
+            res.setHeader('Content-Type', 'application/json')
+            .writeHead(200)
+            .write(response)
+            res.end(response)
         }
 
 })
 
-router.post('/create/:name', async (req, res) => {
-    let newPerson: person = { id: 0, name: req.params.name, address: '', mail: '', phone: 0, rol: 'admin', sex: '' };
+router.post('/create', async (req, res) => {
+    console.log(req.body);
+    let newPerson: person = { id: 0, name: req.body.name, address: req.body.address, mail: req.body.mail, phone: req.body.phone, rol: req.body.rol, sex: req.body.sex };
     let response = await personService.addPerson(newPerson);
-    if(response && req.params.name){
-        res.json(response)
-        .setHeader('Content-Type', 'application/json')
-        .sendStatus(200);
+    if(response){
+        res.setHeader('Content-Type', 'application/json')
+        .writeHead(200)
+        .write(response)
+        res.end(response)
     }
     
 })
--- src/services/personService.ts
+++ src/services/personService.ts
@@ -5,7 +5,7 @@ export const getPerson = async () => {
     try {
         const conn = await connect();
         const inventory =  await conn.query('SELECT * FROM person');
-        const response = inventory[0];
+        const response = JSON.stringify(inventory);
         return response;
     } catch (error) {
         console.log(error);
@@ -17,7 +17,7 @@ export const getPersonById =async (id:number) => {
     try {
         const conn = await connect();
         const inventory =  await conn.query('SELECT * FROM person WHERE id = ' + id);
-        const response = inventory[0];
+        const response = JSON.stringify(inventory);
         return response;
     } catch (error) {
         console.log(error);
@@ -28,8 +28,8 @@ export const getPersonById =async (id:number) => {
 export const getPersonByName =async (name:string) => {
     try {
         const conn = await connect();
-        const inventory =  await conn.query('SELECT * FROM person WHERE name = ' + name);
-        const response = inventory[0];
+        const inventory =  await conn.query('SELECT * FROM person WHERE name = "' + name+'"');
+        const response = JSON.stringify(inventory);
         return response;
     } catch (error) {
         console.log(error);
@@ -41,12 +41,9 @@ export const addPerson = async (inventory: person) => {
     try {
         console.log('inventory '+inventory.name);
         const conn = await connect();
-        let sql = 'INSERT INTO inventory.person (name) VALUE ("'+ inventory.name +'")'
-        console.log(sql);
-        
+        let sql = 'insert into inventory.person(name, phone, mail, sex, role, address) values ("'+inventory.name+'", '+inventory.phone+',"'+inventory.mail+'", "'+inventory.sex+'", "'+inventory.rol+'", "'+inventory.address+'")'
         const queryresponse =  await conn.query(sql);
-        // const response = queryresponse[0];
-        return queryresponse
+        return JSON.stringify(queryresponse)
     } catch (error) {
         console.log(error);
     }
